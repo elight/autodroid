956c4a011de4aab7c2445b7f2824366202a3e95d
elight <evan@tiggerpalace.com>
2009-12-26 13:32:35 -0800

    Fixed layout so that list items take up entire screen width; leaving portrait layout for later

--- src/com/tiggerpalace/automan/PlaylistActivity.java
+++ src/com/tiggerpalace/automan/PlaylistActivity.java
@@ -1,18 +1,24 @@
 package com.tiggerpalace.automan;
 
+import java.io.DataInputStream;
+
 import android.app.ListActivity;
 import android.database.Cursor;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
+import android.widget.ListAdapter;
 import android.widget.SimpleCursorAdapter;
 import android.widget.AdapterView.OnItemSelectedListener;
 
 import static android.provider.BaseColumns._ID;
 import static android.provider.MediaStore.Audio.PlaylistsColumns.NAME;
+import static android.provider.MediaStore.Audio.PlaylistsColumns.DATA;
 
 public class PlaylistActivity extends ListActivity implements OnItemSelectedListener {
   private static final Uri PLAYLIST_URI = MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI;
@@ -21,24 +27,37 @@ public class PlaylistActivity extends ListActivity implements OnItemSelectedList
   private static final int[] TO = { R.id.rowid, R.id.name };
   private static String ORDER_BY = MediaStore.Audio.PlaylistsColumns.NAME + " ASC";
 
+  private int dataColumnIndex;
   
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.playlists);
+    //setContentView(R.layout.playlists);
     getListView().setOnItemSelectedListener(this);
 
-    Cursor playlistsCursor = managedQuery(PLAYLIST_URI, FROM, null, null, ORDER_BY);
-    SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
-          R.layout.item, playlistsCursor, FROM, TO);
+    // Get a cursor with all people
+    Cursor c = getContentResolver().query(PLAYLIST_URI, FROM, null, null, ORDER_BY);
+    startManagingCursor(c);
+    dataColumnIndex= c.getColumnIndex(DATA);    
+    
+    ListAdapter adapter = new SimpleCursorAdapter(this,
+          R.layout.item, c, FROM, TO);
     setListAdapter(adapter);
   }
 
 
-  public void onItemSelected(AdapterView parent, View v, int position, long id) {
+  public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+	Log.d("callback", position + "");
     if (position >= 0) {
         Cursor c = (Cursor) parent.getItemAtPosition(position);
-        throw new RuntimeException("This is where you should be querying for the playlist and then playing it!");
+        MediaPlayer mp = new MediaPlayer();
+        try {
+	        mp.setDataSource(c.getString(dataColumnIndex));
+	        mp.prepare();
+        } catch(Exception e) {
+        	Log.e("playing playlist", e.getMessage());
+        }
+        mp.start();
     }
   }
 
