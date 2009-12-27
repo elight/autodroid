a2e65bb7f59ca67f0aff97565cad75bea7feda72
elight <evan@tiggerpalace.com>
2009-12-26 20:06:48 -0500

    Music now plays sequentially by limiting the player to a Singleton; not sure why the hell the onCreate is invoked multiple times during the life of the app though....

--- src/com/tiggerpalace/automan/MediaPlayerActivity.java
+++ src/com/tiggerpalace/automan/MediaPlayerActivity.java
@@ -21,56 +21,77 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MediaPlayerActivity extends Activity {
-    private Uri playlistURI;
+  private Uri playlistURI;
 
-    private static final String[] FROM = { _ID, DATA };
-	
-	private ArrayList<String> audioFiles = new ArrayList<String>();
+  private static final String[] FROM = { _ID, DATA };
 
-	private int fileNameColumnIndex;
-	
-	private static Thread playerThread;
-	
+  private ArrayList<String> audioFiles = new ArrayList<String>();
+
+  public ArrayList<String> getAudioFiles() {
+    return audioFiles;
+  }
+
+  public void setAudioFiles(ArrayList<String> audioFiles) {
+    setAudioFiles(audioFiles);
+  }
+
+  private int fileNameColumnIndex;
+
+  private static Thread playerThread;
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.mediaplayer);
 
-    String playlistid = getIntent().getStringExtra("PLAYLIST_ID");
-    
-    audioFiles = loadMediaFrom(playlistid);
-    playFilesInOrderFrom(audioFiles);
+    if(getPlayerThread() == null) {    
+      // Why the hell is this getting called more than once when the app is running?
+      String playlistid = getIntent().getStringExtra("PLAYLIST_ID");
+      this.audioFiles = loadMediaFrom(playlistid);
+      playFilesInOrderFrom(getAudioFiles());
+    }
   }
-  
+
   private void playFilesInOrderFrom(ArrayList<String> fileList) {
-	Log.d("AutoDroid:MediaPlayerActivity:playFilesInOrderFrom", fileList.toString());
-	if(playerThread != null) {
-      Log.d("AutoDroid:MediaPlayerActivity:playFilesInOrderFrom", "playerThread already running! interrupting!");	  
-	  playerThread.interrupt();
-	  playerThread = null;
-	}
-	playerThread = new Thread(new PlaylistPlayer(fileList));
-	playerThread.start();
+    Log.d("AutoDroid:MediaPlayerActivity:playFilesInOrderFrom", fileList
+        .toString());
+    if (getPlayerThread() != null) {
+      Log.d("AutoDroid:MediaPlayerActivity:playFilesInOrderFrom",
+          "playerThread already running! interrupting!");
+      getPlayerThread().interrupt();
+      setPlayerThread(null);
+    }
+    setPlayerThread(new Thread(new PlaylistPlayer(fileList)));
+    getPlayerThread().start();
   }
 
   private ArrayList<String> loadMediaFrom(String playlistid) {
-	playlistURI = MediaStore.Audio.Playlists.Members.getContentUri("external", Long.parseLong(playlistid));
-	Cursor c = managedQuery(playlistURI, FROM, null, null, null);
-	if(!c.moveToFirst()) {
-	  Log.w("AutoDroid:MediaPlayerActivity:loadMediaFrom", "Got an empty cursor back WTF?");
-	}
-	fileNameColumnIndex = c.getColumnIndex(DATA);
+    playlistURI = MediaStore.Audio.Playlists.Members.getContentUri("external",
+        Long.parseLong(playlistid));
+    Cursor c = managedQuery(playlistURI, FROM, null, null, null);
+    if (!c.moveToFirst()) {
+      Log.w("AutoDroid:MediaPlayerActivity:loadMediaFrom",
+          "Got an empty cursor back WTF?");
+    }
+    fileNameColumnIndex = c.getColumnIndex(DATA);
 
-	String audioFileName;
-	do {
-	  audioFileName = c.getString(fileNameColumnIndex);
-	  Log.d("AutoDroid:MediaPlayerActivity:loadMediaFrom", audioFileName);
-	  this.audioFiles.add(audioFileName);
-	} while(c.moveToNext());
-	
-	c.close();
-	
-	return audioFiles;
+    String audioFileName;
+    do {
+      audioFileName = c.getString(fileNameColumnIndex);
+      Log.d("AutoDroid:MediaPlayerActivity:loadMediaFrom", audioFileName);
+      this.audioFiles.add(audioFileName);
+    } while (c.moveToNext());
+
+    c.close();
+
+    return audioFiles;
+  }
+
+  private static void setPlayerThread(Thread playerThread) {
+    MediaPlayerActivity.playerThread = playerThread;
+  }
+
+  private static Thread getPlayerThread() {
+    return playerThread;
   }
 }
-
