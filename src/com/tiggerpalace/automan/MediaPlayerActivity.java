e467152246eb86a476bb39577ec128bda175b76f
elight <evan@tiggerpalace.com>
2009-12-26 19:15:09 -0500

    Can now play the first file in a playlist off of the custom media player internals

--- src/com/tiggerpalace/automan/MediaPlayerActivity.java
+++ src/com/tiggerpalace/automan/MediaPlayerActivity.java
@@ -1,24 +1,74 @@
 package com.tiggerpalace.automan;
 
+import static android.provider.BaseColumns._ID;
+import static android.provider.MediaStore.MediaColumns.DATA;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Currency;
+
 import android.app.Activity;
+import android.database.Cursor;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class MediaPlayerActivity extends Activity {
+    private Uri playlistURI;
+
+    private static final String[] FROM = { _ID, DATA };
+	
+	private ArrayList<String> audioFiles = new ArrayList<String>();
+
+	private int fileNameColumnIndex;
+	
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.mediaplayer);
 
-    String m3u = getIntent().getStringExtra("playlist");
+    String playlistid = getIntent().getStringExtra("PLAYLIST_ID");
+    
+    audioFiles = loadMediaFrom(playlistid);
+    playFirstFileIn(audioFiles);
+  }
 
-    // TODO: Now play the playlist!
-    Log.d("Play playlist", m3u);
+  private void playFirstFileIn(ArrayList<String> fileList) {
+	Log.d("Play first file in", fileList.toString());
+	MediaPlayer player = new MediaPlayer();
+	try {
+	  player.setDataSource(fileList.get(0));
+	  player.prepare();
+	  player.start();
+	} catch(Exception e) {
+	  Log.e("playFirstFileIn", e.getMessage());
+	}
+  }
 
-    TextView textView = (TextView) findViewById(R.id.playlist); 
-    textView.setText(m3u);
+  private ArrayList<String> loadMediaFrom(String playlistid) {
+	playlistURI = MediaStore.Audio.Playlists.Members.getContentUri("external", Long.parseLong(playlistid));
+	Cursor c = managedQuery(playlistURI, FROM, null, null, null);
+	if(!c.moveToFirst()) {
+	  Log.w("loadMediaFrom", "Got an empty cursor back WTF?");
+	}
+	fileNameColumnIndex = c.getColumnIndex(DATA);
+
+	String audioFileName;
+	do {
+	  audioFileName = c.getString(fileNameColumnIndex);
+	  Log.d("loadMediaFrom", audioFileName);
+	  this.audioFiles.add(audioFileName);
+	} while(c.moveToNext());
+	
+	c.close();
+	
+	return audioFiles;
   }
 }
 
--- src/com/tiggerpalace/automan/PlaylistActivity.java
+++ src/com/tiggerpalace/automan/PlaylistActivity.java
@@ -1,12 +1,6 @@
 package com.tiggerpalace.automan;
 
-import java.io.DataInputStream;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
-
 import android.app.ListActivity;
-import android.content.ComponentName;
 import android.content.Intent;
 import android.database.Cursor;
 import android.media.MediaPlayer;
@@ -15,25 +9,21 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
-import android.widget.AdapterView.OnItemSelectedListener;
 
 import static android.provider.BaseColumns._ID;
 import static android.provider.MediaStore.Audio.PlaylistsColumns.NAME;
-import static android.provider.MediaStore.Audio.PlaylistsColumns.DATA;
 
 public class PlaylistActivity extends ListActivity {
   private static final Uri PLAYLIST_URI = MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI;
 
-  private static final String[] FROM = { _ID, NAME, DATA };
+  private static final String[] FROM = { _ID, NAME };
   private static final int[] TO = { R.id.rowid, R.id.name };
   private static String ORDER_BY = MediaStore.Audio.PlaylistsColumns.NAME + " ASC";
 
-  private int dataColumnIndex;
+  private int idColumnIndex;
   
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -43,21 +33,22 @@ public class PlaylistActivity extends ListActivity {
     // Get a cursor with all people
     Cursor c = getContentResolver().query(PLAYLIST_URI, FROM, null, null, ORDER_BY);
     startManagingCursor(c);
-    this.dataColumnIndex = c.getColumnIndex(DATA);
-
-    ListAdapter adapter = new SimpleCursorAdapter(this, R.layout.item, c, FROM, TO);
+    this.idColumnIndex = c.getColumnIndex(_ID);
+    
+    ListAdapter adapter = new SimpleCursorAdapter(this,
+          R.layout.item, c, FROM, TO);
     setListAdapter(adapter);
   }
 
   @Override
   public void onListItemClick(ListView parent, View v, int position, long id) {
-  Log.d("onListItemClick", id + "");  
+	Log.d("onListItemClick", id + "");  
     if (position >= 0) {
-      Cursor c = (Cursor) parent.getItemAtPosition(position);
-      String m3u = c.getString(this.dataColumnIndex);
-      Intent intent = new Intent(this, MediaPlayerActivity.class); 
-      intent.putExtra("playlist", m3u); 
-      startActivity(intent);
+        Cursor c = (Cursor) parent.getItemAtPosition(position);
+        String playlistid = c.getString(this.idColumnIndex);
+        Intent intent = new Intent(this, MediaPlayerActivity.class); 
+        intent.putExtra("PLAYLIST_ID", playlistid); 
+        startActivity(intent);
     }
   }
 }
